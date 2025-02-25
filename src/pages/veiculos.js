03307c18f99ef645c8802dbf628e67aa0f7406e4
tais defante <taisadefante@hotmail.com>
2025-02-25 14:41:26 -0300

    a

--- src/pages/veiculos.js
+++ src/pages/veiculos.js
@@ -1,4 +1,4 @@
-aimport React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { Modal, Button, Carousel, Form } from "react-bootstrap";
 import { FaWhatsapp, FaFilter } from "react-icons/fa";
 import API_BASE_URL from "../services/api";
@@ -32,7 +32,8 @@ function Veiculos() {
       const res = await fetch(`${API_BASE_URL}/api/vehicles`);
       if (!res.ok) throw new Error(`Erro ao buscar veículos: ${res.status}`);
       const data = await res.json();
-      if (!Array.isArray(data)) throw new Error("Resposta da API não é um array.");
+      if (!Array.isArray(data))
+        throw new Error("Resposta da API não é um array.");
       setVeiculos(data);
       setFilteredVehicles(data);
     } catch (error) {
@@ -50,16 +51,26 @@ function Veiculos() {
   const applyFilters = () => {
     let filtered = veiculos.filter((veiculo) => {
       return (
-        (filters.name === "" || veiculo.carName.toLowerCase().includes(filters.name.toLowerCase())) &&
-        (filters.model === "" || veiculo.model.toLowerCase().includes(filters.model.toLowerCase())) &&
-        (filters.brand === "" || veiculo.brand.toLowerCase().includes(filters.brand.toLowerCase())) &&
-        (filters.color === "" || veiculo.color.toLowerCase().includes(filters.color.toLowerCase())) &&
-        (filters.minYear === "" || parseInt(veiculo.year) >= parseInt(filters.minYear)) &&
-        (filters.maxYear === "" || parseInt(veiculo.year) <= parseInt(filters.maxYear)) &&
-        (filters.minPrice === "" || parseFloat(veiculo.price) >= parseFloat(filters.minPrice)) &&
-        (filters.maxPrice === "" || parseFloat(veiculo.price) <= parseFloat(filters.maxPrice)) &&
-        (filters.minKm === "" || parseInt(veiculo.mileage) >= parseInt(filters.minKm)) &&
-        (filters.maxKm === "" || parseInt(veiculo.mileage) <= parseInt(filters.maxKm))
+        (filters.name === "" ||
+          veiculo.carName.toLowerCase().includes(filters.name.toLowerCase())) &&
+        (filters.model === "" ||
+          veiculo.model.toLowerCase().includes(filters.model.toLowerCase())) &&
+        (filters.brand === "" ||
+          veiculo.brand.toLowerCase().includes(filters.brand.toLowerCase())) &&
+        (filters.color === "" ||
+          veiculo.color.toLowerCase().includes(filters.color.toLowerCase())) &&
+        (filters.minYear === "" ||
+          parseInt(veiculo.year) >= parseInt(filters.minYear)) &&
+        (filters.maxYear === "" ||
+          parseInt(veiculo.year) <= parseInt(filters.maxYear)) &&
+        (filters.minPrice === "" ||
+          parseFloat(veiculo.price) >= parseFloat(filters.minPrice)) &&
+        (filters.maxPrice === "" ||
+          parseFloat(veiculo.price) <= parseFloat(filters.maxPrice)) &&
+        (filters.minKm === "" ||
+          parseInt(veiculo.mileage) >= parseInt(filters.minKm)) &&
+        (filters.maxKm === "" ||
+          parseInt(veiculo.mileage) <= parseInt(filters.maxKm))
       );
     });
     setFilteredVehicles(filtered);
@@ -67,7 +78,9 @@ function Veiculos() {
 
   const handleShowModal = (veiculo) => {
     setSelectedVehicle(veiculo);
-    setMainImage(veiculo.images.length > 0 ? `${API_BASE_URL}${veiculo.images[0]}` : "");
+    setMainImage(
+      veiculo.images.length > 0 ? `${API_BASE_URL}${veiculo.images[0]}` : ""
+    );
     setShowModal(true);
   };
 
@@ -80,27 +93,102 @@ function Veiculos() {
   return (
     <section style={{ padding: "40px 0", backgroundColor: "#f8f9fa" }}>
       <div className="container">
-        <h2 className="text-center fs-3 mb-4" style={{ fontWeight: "bold", color: "#333" }}>Todos os Veículos 🚗💨</h2>
+        <h2
+          className="text-center fs-3 mb-4"
+          style={{ fontWeight: "bold", color: "#333" }}
+        >
+          Todos os Veículos 🚗💨
+        </h2>
 
-        <Button variant="dark" className="d-md-none mb-3" onClick={() => setShowFilters(!showFilters)}>
+        <Button
+          variant="dark"
+          className="d-md-none mb-3"
+          onClick={() => setShowFilters(!showFilters)}
+        >
           <FaFilter /> Filtrar Veículos
         </Button>
 
         <div className="row">
-          <aside className={`col-lg-3 col-md-4 mb-4 ${showFilters ? "d-block" : "d-none d-md-block"}`}>
+          <aside
+            className={`col-lg-3 col-md-4 mb-4 ${
+              showFilters ? "d-block" : "d-none d-md-block"
+            }`}
+          >
             <div className="p-3 bg-white shadow-sm rounded">
               <h5>Filtrar Veículos</h5>
-              <Form.Control type="text" name="name" placeholder="Nome" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="text" name="model" placeholder="Modelo" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="text" name="brand" placeholder="Marca" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="text" name="color" placeholder="Cor" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="number" name="minPrice" placeholder="Menor Valor" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="number" name="maxPrice" placeholder="Maior Valor" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="number" name="minYear" placeholder="Ano Inicial" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="number" name="maxYear" placeholder="Ano Final" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="number" name="minKm" placeholder="Menor KM" className="mb-2" onChange={handleFilterChange} />
-              <Form.Control type="number" name="maxKm" placeholder="Maior KM" className="mb-2" onChange={handleFilterChange} />
-              <Button variant="dark" className="w-100" onClick={applyFilters}>Pesquisar</Button>
+              <Form.Control
+                type="text"
+                name="name"
+                placeholder="Nome"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="text"
+                name="model"
+                placeholder="Modelo"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="text"
+                name="brand"
+                placeholder="Marca"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="text"
+                name="color"
+                placeholder="Cor"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="number"
+                name="minPrice"
+                placeholder="Menor Valor"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="number"
+                name="maxPrice"
+                placeholder="Maior Valor"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="number"
+                name="minYear"
+                placeholder="Ano Inicial"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="number"
+                name="maxYear"
+                placeholder="Ano Final"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="number"
+                name="minKm"
+                placeholder="Menor KM"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Form.Control
+                type="number"
+                name="maxKm"
+                placeholder="Maior KM"
+                className="mb-2"
+                onChange={handleFilterChange}
+              />
+              <Button variant="dark" className="w-100" onClick={applyFilters}>
+                Pesquisar
+              </Button>
             </div>
           </aside>
 
@@ -112,10 +200,20 @@ function Veiculos() {
                 {filteredVehicles.map((veiculo) => (
                   <div key={veiculo.id} className="col-md-4 mb-4">
                     <div className="card shadow-sm h-100">
-                      <img src={`${API_BASE_URL}${veiculo.images[0]}`} className="card-img-top" alt={veiculo.carName} />
+                      <img
+                        src={`${API_BASE_URL}${veiculo.images[0]}`}
+                        className="card-img-top"
+                        alt={veiculo.carName}
+                      />
                       <div className="card-body text-center">
                         <h5>{veiculo.carName}</h5>
-                        <Button variant="dark" size="sm" onClick={() => handleShowModal(veiculo)}>Detalhes</Button>
+                        <Button
+                          variant="dark"
+                          size="sm"
+                          onClick={() => handleShowModal(veiculo)}
+                        >
+                          Detalhes
+                        </Button>
                       </div>
                     </div>
                   </div>
